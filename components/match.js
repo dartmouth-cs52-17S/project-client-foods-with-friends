417d6bb1238b5ac0eefc0658320311f86168fa6a
Jane Lee <jane.lee.19@dartmouth.edu>
2017-05-17 21:52:36 -0400

    travis please work

--- components/match.js
+++ components/match.js
@@ -10,63 +10,7 @@ import {
   ScrollView,
 } from 'react-native';
 
-class Match extends React.Component {
-  static defaultProps = {
-    date: new Date(),
-    timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
-  };
-
-  state = {
-    date: this.props.date,
-    timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
-  };
-
-  onDateChange = (date) => {
-    this.setState({ date });
-  };
-
-  onTimezoneChange = (event) => {
-    const offset = parseInt(event.nativeEvent.text, 10);
-    if (isNaN(offset)) {
-      return;
-    }
-    this.setState({ timeZoneOffsetInHours: offset });
-  };
-
-  handlePress() {
-    console.log('Pressed!');
-  }
-
-  render() {
-    return (
-      <ScrollView style={styles.body}>
-        <View>
-          <Text style={styles.timeLabel}>Pick a time:</Text>
-          <DatePickerIOS
-            date={this.state.date}
-            mode="time"
-            timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
-            onDateChange={this.onDateChange}
-            minuteInterval={15}
-          />
-          <Text style={styles.topicLabel}>Pick a conversation topic:</Text>
-          <TextInput
-            placeholder="Enter topic"
-            style={styles.topic}
-          />
-          <Button1
-            style={styles.button}
-            textStyle={styles.buttonText}
-          >
-          Match Me!
-          </Button1>
-        </View>
-      </ScrollView>
-    );
-  }
-}
-
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   button: {
     marginTop: 50,
     backgroundColor: '#519bdd',
@@ -124,5 +68,61 @@ var styles = StyleSheet.create({
     fontSize: 14,
   },
 });
+
+class Match extends React.Component {
+  static defaultProps = {
+    date: new Date(),
+    timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
+  };
+
+  state = {
+    date: this.props.date,
+    timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
+  };
+
+  onDateChange = (date) => {
+    this.setState({ date });
+  };
+
+  onTimezoneChange = (event) => {
+    const offset = parseInt(event.nativeEvent.text, 10);
+    if (isNaN(offset)) {
+      return;
+    }
+    this.setState({ timeZoneOffsetInHours: offset });
+  };
+
+  handlePress() {
+    console.log('Pressed!');
+  }
+
+  render() {
+    return (
+      <ScrollView style={styles.body}>
+        <View>
+          <Text style={styles.timeLabel}>Pick a time:</Text>
+          <DatePickerIOS
+            date={this.state.date}
+            mode="time"
+            timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+            onDateChange={this.onDateChange}
+            minuteInterval={15}
+          />
+          <Text style={styles.topicLabel}>Pick a conversation topic:</Text>
+          <TextInput
+            placeholder="Enter topic"
+            style={styles.topic}
+          />
+          <Button1
+            style={styles.button}
+            textStyle={styles.buttonText}
+          >
+          Match Me!
+          </Button1>
+        </View>
+      </ScrollView>
+    );
+  }
+}
 
 module.exports = Match;
