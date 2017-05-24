f9290ee411226692e0b4c2e7399ea2d2bce3dd56
Abby Starr <astarr2015@gmail.com>
2017-05-23 22:47:11 -0400

    added matchHistory page and included in tab bar

---
+++ components/matchHistory.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  ScrollView,
+  NavigatorIOS,
+} from 'react-native';
+
+import { signoutUser } from '../actions';
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  username: {
+    marginTop: 25,
+    fontSize: 30,
+    textAlign: 'center',
+    color: '#519bdd',
+    fontWeight: 'bold',
+    fontFamily: 'Avenir Next',
+  },
+  text: {
+    textAlign: 'center',
+    marginTop: 7,
+    fontSize: 14,
+    fontFamily: 'Avenir Next',
+  },
+});
+
+class MatchHistory extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={styles.body}>
+          <Text style={styles.username}>Match History</Text>
+          <FlatList
+            data={[{ title: 'June 21, 4:00pm' }, { title: 'June 22, 5:30pm' }]}
+            renderItem={({ item }) => <Text style={styles.text}>{item.title}</Text>}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+export default class MatchHistoryNav extends Component {
+  static navigationOptions = {
+    tabBarLabel: 'Match',
+  };
+  render() {
+    return (
+      <NavigatorIOS
+        initialRoute={{
+          component: MatchHistory,
+          title: 'Match History',
+        }}
+        style={{ flex: 1 }}
+      />
+    );
+  }
+}
--- components/tab.js
+++ components/tab.js
@@ -3,10 +3,12 @@ import { TabNavigator } from 'react-navigation';
 
 import Match from '../containers/match';
 import Profile from '../components/profiletab';
+import MatchHistory from '../components/matchHistory';
 
 const MunchBuddyTabs = TabNavigator({
   Match: { screen: Match },
   Profile: { screen: Profile },
+  Hsitory: { screen: MatchHistory },
 });
 
 export default MunchBuddyTabs;
