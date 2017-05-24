8c1b94eccb2f957f59438393d71c20e8b14d35a0
Abby Starr <astarr2015@gmail.com>
2017-05-23 23:11:38 -0400

    added chat page and tab

--- components/tab.js
+++ components/tab.js
@@ -4,11 +4,13 @@ import { TabNavigator } from 'react-navigation';
 import Match from '../containers/match';
 import Profile from '../components/profiletab';
 import MatchHistory from '../components/matchHistory';
+import Chat from '../components/chat';
 
 const MunchBuddyTabs = TabNavigator({
   Match: { screen: Match },
   Profile: { screen: Profile },
-  Hsitory: { screen: MatchHistory },
+  History: { screen: MatchHistory },
+  Chat: { screen: Chat },
 });
 
 export default MunchBuddyTabs;
