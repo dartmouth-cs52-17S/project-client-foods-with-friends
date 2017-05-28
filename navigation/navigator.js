6839c1c9ec64f10dc60e4aa51f74fe5642b800b8
Allenwu <chimian.wu@gmail.com>
2017-05-28 01:45:56 -0400

    add npm pacakge and hello test for socketio

--- navigation/navigator.js
+++ navigation/navigator.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import { View, StyleSheet } from 'react-native';
+import SocketIOClient from 'socket.io-client';
 import SignUp from '../containers/signup';
 import SignIn from '../containers/signin';
 import { signoutUser } from '../actions';
@@ -21,6 +21,10 @@ class Navigator extends Component {
     this.state = { title: 'Sign Up', component: SignUp };
 
     this.renderPage = this.renderPage.bind(this);
+
+    // NOTE: change the url after server is deployed. TESTING socket io locally
+    this.socket = SocketIOClient('http://localhost:9090');
+    this.socket.emit('hello', 'Hello from client side');
   }
 
   renderPage() {
