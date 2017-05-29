46b02cf7f7b28172cc58abc9b7ef9ec56883b6e1
Allenwu <chimian.wu@gmail.com>
2017-05-29 11:14:07 -0400

    pulling  chat history. allow chat with anyusr given its userID.

--- components/chatPage.js
+++ components/chatPage.js
@@ -3,8 +3,6 @@ import { StyleSheet, View, ScrollView, Text, AsyncStorage } from 'react-native';
 import SocketIOClient from 'socket.io-client';
 import { GiftedChat } from 'react-native-gifted-chat';
 
-const USER_ID = '@userId';
-
 const styles = StyleSheet.create({
   chat: {
     flex: 1,
@@ -26,39 +24,28 @@ class ChatPage extends React.Component {
             name: 'React Native',
             avatar: 'https://facebook.github.io/react/img/logo_og.png',
           },
+          roomID: 1,
         },
       ],
     };
 
+  // NOTE: Hard code a matched person here.
+  // HACK: Please fill in the matchedUserID here
+    this.matchedUserID = '5928deae7cb4d4216ad6580e';
+    this.myUserID = '';
+    this.roomID = '';
     // this.determineUser = this.determineUser.bind(this);
     this.onReceivedMessage = this.onReceivedMessage.bind(this);
     this.onSend = this.onSend.bind(this);
     this._storeMessages = this._storeMessages.bind(this);
-
+    this.onReceivedExistingMessage = this.onReceivedExistingMessage.bind(this);
+    this.convertUserFormat = this.convertUserFormat.bind(this);
 
     this.socket = SocketIOClient('http://localhost:9090');
 
     // this.socket = SocketIOClient('https://munchbuddy.herokuapp.com');
     this.socket.on('message', this.onReceivedMessage);
-    // this.determineUser();
-  }
-
-  // Event listeners
-  /**
-   * When the server sends a message to this.
-   */
-  onReceivedMessage(messages) {
-    this._storeMessages(messages);
-  }
-
-  /**
-   * When a message is sent, send the message to the server
-   * and store it in this component's state.
-   */
-  onSend(messages = []) {
-    // this.socket.emit('message', messages[0]);
-    this.socket.emit('hello', 'messages[0]');
-    this._storeMessages(messages);
+    this.socket.on('exisitingMsgsResult', this.onReceivedExistingMessage);
   }
 
   componentDidMount() {
@@ -72,6 +59,11 @@ class ChatPage extends React.Component {
               .emit('authenticate', { token: User }) // send the jwt token
               .on('authenticated', () => {
                 console.log('Yo, i am authorized in chat page!');
+                this.socket.on('getMyUserID', (id) => {
+                  console.log(`In get my user ID event, my id = ${id}`);
+                  this.myUserID = id;
+                  this.socket.emit('getExistingMsgs', this.matchedUserID);
+                });
               })
               .on('unauthorized', (msg) => {
                 console.log(`unauthorized: ${JSON.stringify(msg.data)}`);
@@ -83,6 +75,48 @@ class ChatPage extends React.Component {
       console.log(err);
     });
   }
+
+  // Event listeners
+  onReceivedExistingMessage(result) {
+    console.log(`onReceivedExistingMessage ${JSON.stringify(result)}`);
+    this.roomID = result.roomID;
+
+    this.onReceivedMessage(this.convertUserFormat(result.messages));
+  }
+  /**
+   * When the server sends a message to this.
+   */
+  onReceivedMessage(messages) {
+    console.log(`onReceivedMessage ${JSON.stringify(messages)}`);
+    this._storeMessages(messages);
+  }
+
+  /**
+   * When a message is sent, send the message to the server
+   * and store it in this component's state.
+   */
+  onSend(messages = []) {
+    // this.socket.emit('message', messages[0]);
+    // TODO: Unify the dateofTime for giftedChat and mongoose
+    const formatedTime = messages[0].createdAt.toISOString();
+    const msgToSend = messages[0];
+    msgToSend.createdAt = formatedTime;
+    msgToSend.roomID = this.roomID;
+    console.log(`In onSend, message = ${messages[0].text}, userID = ${messages[0].user._id}, createdAt = ${messages[0].createdAt}, formattedTime = ${formatedTime}, rooomID = ${msgToSend.roomID}`);
+    this.socket.emit('message', msgToSend);
+    this._storeMessages(messages);
+  }
+
+  // HACK: Format the user format in message to be user {_id} so giftedChat can show
+  convertUserFormat(messages) {
+    const convertedMsg = messages;
+    for (let i = 0; i < convertedMsg.length; i += 1) {
+      convertedMsg[i].user = { _id: messages[i].user };
+    }
+    console.log(`roomID = ${this.roomID}, convertedMsg = ${JSON.stringify(convertedMsg)}`);
+    return convertedMsg;
+  }
+
   /**
    * When a user joins the chatroom, check if they are an existing user.
    * If they aren't, then ask the server for a userId.
@@ -117,7 +151,7 @@ class ChatPage extends React.Component {
   }
 
   render() {
-    const user = AsyncStorage.getItem('token');
+    // const user = AsyncStorage.getItem('token');
 
     return (
       <GiftedChat
@@ -125,7 +159,7 @@ class ChatPage extends React.Component {
         messages={this.state.messages}
         onSend={this.onSend}
         user={{
-          _id: 4,
+          _id: this.myUserID,
         }}
       />
     );
